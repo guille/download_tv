b8565e9f458bfde4d56a828bab87120d8cff5128
guille <guille10000@gmail.com>
2016-11-10 03:14:16 +0100

    Improved tests

--- Rakefile
+++ Rakefile
@@ -3,5 +3,5 @@ require 'rake/testtask'
 Rake::TestTask.new do |t|
 	t.libs << "test"
 	t.test_files = FileList['test/test*.rb']
-	t.verbose = true
+	t.verbose = false
 end
--- test/test_downloader.rb
+++ test/test_downloader.rb
@@ -1,33 +1,37 @@
-require 'minitest/spec'
 require 'minitest/autorun'
-require 'date'
+# require 'date'
 require_relative '../downloader'
+
+Dir.chdir(File.dirname(__FILE__))
 
 describe ShowDownloader::Downloader do
 
-	before do
-		Dir.chdir(File.dirname(__FILE__))
-	end
-
 	describe "when creating the object" do
-		it "should store the first parameter as @offset" do
+		it "should store the first argument as @offset" do
 			ShowDownloader::Downloader.new("foo").offset.must_equal 0
 			ShowDownloader::Downloader.new(3).offset.must_equal 3
+		end
+
+		it "should set offset to 0 if no argument is given" do
 			ShowDownloader::Downloader.new.offset.must_equal 0
 		end
 
-		# it "should raise an error when no arguments are given" do
-		#   -> {ShowDownloader::Downloader.new}.must_raise(ArgumentError)
-		#   # -> {ShowDownloader::Downloader.new([])}.must_raise(ArgumentError)
-		# end
 	end
 
-	it "should remove apostrophes and parens" do
-		shows = ["Mr. Foo S01E02", "Bar (UK) S00E22", "Ignored S20E22", "Let's S05E03"]
-		result = ["Mr. Foo S01E02", "Bar UK S00E22", "Lets S05E03"]
-		ShowDownloader::Downloader.new.fix_names(shows).must_equal result
-			
+	describe "the fix_names function" do
+		it "should remove apostrophes and parens" do
+			shows = ["Mr. Foo S01E02", "Bar (UK) S00E22", "Let's S05E03"]
+			result = ["Mr. Foo S01E02", "Bar UK S00E22", "Lets S05E03"]
+			ShowDownloader::Downloader.new.fix_names(shows).must_equal result
+		end
+
+		it "should remove ignored shows" do
+			shows = ["Mr. Foo S01E02", "Bar (UK) S00E22", "Ignored S20E22", "Let's S05E03"]
+			result = ["Mr. Foo S01E02", "Bar UK S00E22", "Lets S05E03"]
+			ShowDownloader::Downloader.new.fix_names(shows).must_equal result
+		end
 	end
+
 
 	describe "the date file" do 
 		dl = ShowDownloader::Downloader.new
@@ -42,22 +46,21 @@ describe ShowDownloader::Downloader do
 		end
 
 		it "contains a date after running the method" do
-			flag, date = dl.check_date
-			flag.must_equal false
+			date = dl.check_date
 			date.must_equal (Date.today-1)
 			Date.parse(File.read("date")).must_equal Date.today-1
 		end
+
+		it "exits the script when up to date" do
+			File.write("date", Date.today)
+			begin
+				dl.check_date
+				flunk
+			rescue SystemExit
+			
+			end
+		end
 		
 	end
-
-	# must raise AuthenticationError
-	# random string must raise torrent not found
-	# also, add it to the @@pending array
-	# querying eztv and torrentapi should return 200 OK
-
-	# it has to be called from /test
-	# otherwise it will use the wrong date/ignored files
-	# possible solution: Dir.chdir before running the test
-
 
 end
