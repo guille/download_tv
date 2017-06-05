17c5274e7a6474f8d757460aa7d1b388dde45617
guille <guille10000@gmail.com>
2017-06-05 18:34:36 +0200

    Added tests for new parens behavior

--- test/test_downloader.rb
+++ test/test_downloader.rb
@@ -8,13 +8,12 @@ describe ShowDownloader::Downloader do
 	end
 
 	describe "when creating the object" do
-		it "should store the first argument as @offset" do
-			ShowDownloader::Downloader.new("foo").offset.must_equal 0
-			ShowDownloader::Downloader.new(3).offset.must_equal 3
+		it "should receive an integer" do
+			->{ ShowDownloader::Downloader.new("foo") }.must_raise NoMethodError
 		end
 
-		it "should set offset to 0 if no argument is given" do
-			ShowDownloader::Downloader.new.offset.must_equal 0
+		it "should store the first argument as @offset" do
+			ShowDownloader::Downloader.new(3).offset.must_equal 3
 		end
 
 	end
@@ -22,21 +21,21 @@ describe ShowDownloader::Downloader do
 	describe "the fix_names function" do
 		it "should remove apostrophes and parens" do
 			shows = ["Mr. Foo S01E02", "Bar (UK) S00E22", "Let's S05E03"]
-			result = ["Mr. Foo S01E02", "Bar UK S00E22", "Lets S05E03"]
-			ShowDownloader::Downloader.new.fix_names(shows).must_equal result
+			result = ["Mr. Foo S01E02", "Bar S00E22", "Lets S05E03"]
+			ShowDownloader::Downloader.new(0).fix_names(shows).must_equal result
 		end
 
 		it "should remove ignored shows" do
 			ShowDownloader::CONFIG[:ignored] = ["Ignored"]
 			shows = ["Mr. Foo S01E02", "Bar (UK) S00E22", "Ignored S20E22", "Let's S05E03"]
-			result = ["Mr. Foo S01E02", "Bar UK S00E22", "Lets S05E03"]
-			ShowDownloader::Downloader.new.fix_names(shows).must_equal result
+			result = ["Mr. Foo S01E02", "Bar S00E22", "Lets S05E03"]
+			ShowDownloader::Downloader.new(0).fix_names(shows).must_equal result
 		end
 	end
 
 
 	describe "the date file" do 
-		dl = ShowDownloader::Downloader.new
+		dl = ShowDownloader::Downloader.new(0)
 
 
 
