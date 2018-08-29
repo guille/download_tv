9347a86632206b54ba7871af455f1559e79eaf14
guille <guille@users.noreply.github.com>
2018-08-29 22:22:44 +0200

    Fixed tests

--- test/torrent_test.rb
+++ test/torrent_test.rb
@@ -1,17 +1,18 @@
 require 'test_helper'
 
 describe DownloadTV::Torrent do
+  grabber_names = DownloadTV::Torrent.new(nil).grabbers
   describe 'when creating the object' do
     before do
       @t = DownloadTV::Torrent.new
     end
 
     it 'will populate the instances' do
-      @t.g_instances.size.must_equal @t.grabbers.size
+      @t.g_instances.size.must_equal grabber_names.size
     end
 
     it 'will start with all tries available' do
-      @t.tries.must_equal @t.grabbers.size - 1
+      @t.tries.must_equal grabber_names.size - 1
     end
 
     it 'will call get_links on its grabber' do
@@ -24,11 +25,11 @@ describe DownloadTV::Torrent do
   describe 'when giving it a default grabber' do
     it 'has a default order' do
       t = DownloadTV::Torrent.new(nil)
-      expected = t.grabbers.map { |i| "DownloadTV::#{i}"}
+      expected = grabber_names.map { |i| "DownloadTV::#{i}"}
       t.g_instances.map { |i| i.class.name}.must_equal expected
     end
 
-    %w[Eztv KAT ThePirateBay TorrentAPI].each do |g|
+    grabber_names.each do |g|
       it 'correctly uses the given grabber first' do
         t = DownloadTV::Torrent.new(g)
         t.g_instances.first.class.name.must_equal "DownloadTV::#{g}"
