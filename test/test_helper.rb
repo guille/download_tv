f962e9bef31660e5072a9a34d3140a63b0f22b45
guille <guille@users.noreply.github.com>
2021-05-24 05:05:18 +0200

    Revert "Add Codecov for code coverage"
    This reverts commit 68420ec8e904606e9b1b85df40655778135c6078.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,4 @@
 # frozen_string_literal: true
-require 'simplecov'
-SimpleCov.start
-
-require 'codecov'
-SimpleCov.formatter = SimpleCov::Formatter::Codecov
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'download_tv'
