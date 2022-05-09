202401437a6d2c7f74bd33df4ff0b9a56fe88593
guille <guille@users.noreply.github.com>
2022-05-09 19:52:10 +0200

    Refactor Configuration class and create specs

--- lib/download_tv/configuration.rb
+++ lib/download_tv/configuration.rb
@@ -4,67 +4,147 @@ module DownloadTV
   ##
   # Class used for managing the configuration of the application
   class Configuration
-    attr_reader :content, :config_path
+    def initialize(user_config = {})
+      load_config(user_config)
+    end
 
-    def initialize(content = {})
-      @config_path = content[:path] || default_config_path
-      FileUtils.mkdir_p(File.expand_path('..', @config_path))
+    def [](key)
+      @content[key]
+    end
 
-      if File.exist? @config_path
-        load_config
-        @content.merge!(content) unless content.empty?
-        @content[:ignored]&.map!(&:downcase)
-      else
-        @content = content
-        change_configuration
-      end
+    def []=(key, value)
+      @content[key] = value
     end
 
     def change_configuration
+      prompt_for_new_values
+      set_default_values
+      serialize
+    end
+
+    def serialize
+      self[:pending] = self[:pending].uniq
+      File.write(config_path, JSON.generate(@content))
+    end
+
+    def to_s
+      @content.reduce('') do |mem, item|
+        key, val = item
+        "#{mem}#{key}: #{val}\n"
+      end
+    end
+
+    def clear_pending
+      self[:pending].clear
+      serialize
+    end
+
+    def queue_pending(show)
+      self[:pending] << show
+      serialize
+    end
+
+    private
+
+    def content
+      @content ||= {}
+    end
+
+    def load_config(user_config)
+      if File.exist? config_path
+        parse_config
+      else
+        FileUtils.mkdir_p(File.expand_path('..', config_path))
+        change_configuration
+      end
+      content.merge!(user_config) unless user_config.empty?
+      self[:ignored]&.map!(&:downcase)
+    end
+
+    def parse_config
+      source = File.read(config_path)
+      @content = JSON.parse(source, symbolize_names: true)
+
+      self[:date] = Date.parse(self[:date]) if self[:date]
+
+      if !self[:version] || breaking_changes?(self[:version])
+        warn 'Change configuration required (version with breaking changes detected)'
+        change_configuration
+      end
+    rescue JSON::ParserError => e
+      warn "Error parsing config file at #{config_path} => #{e.message}"
+      change_configuration
+    end
+
+    ##
+    # Returns true if a major or minor update has been detected, or if the config version is newer
+    # than the installed version. Returns something falsy otherwise
+    def breaking_changes?(version)
+      paired = DownloadTV::VERSION.split('.')
+                                  .map(&:to_i)
+                                  .zip(version.split('.').map(&:to_i))
+      # The configuration belongs to a newer version than is installed
+      return true unless paired.find_index { |x, y| x < y }.nil?
+
+      paired.find_index { |x, y| y < x }&.< 2
+    end
+
+    def prompt_for_new_values
       prompt_for_myep_user
       prompt_for_cookie
       prompt_for_ignored
       prompt_for_filters
       $stdout.flush
-
-      set_default_values
-      serialize
     end
 
     def prompt_for_myep_user
-      existing = "(#{@content[:myepisodes_user]}) " if @content[:myepisodes_user]
+      existing = "(#{self[:myepisodes_user]}) " if self[:myepisodes_user]
       print "Enter your MyEpisodes username #{existing}: "
       input = $stdin.gets.chomp
-      @content[:myepisodes_user] = input if input
+      self[:myepisodes_user] = input if input
     end
 
     def prompt_for_cookie
       print 'Save cookie? (y)/n: '
-      @content[:cookie] = !($stdin.gets.chomp.casecmp? 'n')
+      self[:cookie] = !($stdin.gets.chomp.casecmp? 'n')
     end
 
     def prompt_for_ignored
-      existing = "(#{@content[:ignored]})" if @content[:ignored]
+      existing = "(#{self[:ignored]})" if self[:ignored]
       puts "Enter a comma-separated list of shows to ignore: #{existing}"
 
-      @content[:ignored] = read_and_split_list :downcase
+      self[:ignored] = read_and_split_list :downcase
     end
 
     def prompt_for_filters
-      puts "Current filters: (#{@content[:filters]})" if @content[:filters]
-      @content[:filters] = {}
+      puts "Current filters: (#{self[:filters]})" if self[:filters]
+      self[:filters] = {}
 
       puts 'Enter a comma-separated list of terms to include: '
-      @content[:filters][:includes] = read_and_split_list :upcase
+      self[:filters][:includes] = read_and_split_list :upcase
 
       puts 'Enter a comma-separated list of terms to exclude: '
-      @content[:filters][:excludes] = read_and_split_list :upcase
+      self[:filters][:excludes] = read_and_split_list :upcase
     end
 
-    def read_and_split_list(case_method)
-      $stdin.gets.chomp.split(',')
-            .map(&:strip)
-            .map(&case_method)
+    def config_path
+      (content[:path] || default_config_path)
+    end
+
+    def default_config_path
+      File.join(ENV['HOME'], '.config', 'download_tv', 'config')
+    end
+
+    ##
+    # Update the +content+ attribute with the defaults, if needed.
+    # Maintains the previous values, in case it's an update from an existing file.
+    def set_default_values
+      self[:auto] ||= true
+      self[:grabber] ||= 'TorrentAPI'
+      self[:date] ||= Date.today - 1
+      self[:filters] ||= default_filters
+      self[:pending] ||= []
+      self[:version] = DownloadTV::VERSION
     end
 
     def default_filters
@@ -74,64 +154,10 @@ module DownloadTV
       }
     end
 
-    ##
-    # Update the +content+ attribute with the defaults, if needed.
-    # Maintains the previous values, in case it's an update from an existing file.
-    def set_default_values
-      @content[:auto] ||= true
-      @content[:grabber] ||= 'TorrentAPI'
-      @content[:date] ||= Date.today - 1
-      @content[:filters] ||= default_filters
-      @content[:pending] ||= []
-      @content[:version] = DownloadTV::VERSION
-    end
-
-    def serialize
-      @content[:pending] = @content[:pending].uniq
-      File.write(@config_path, JSON.generate(@content))
-    end
-
-    def load_config
-      source = File.read(@config_path)
-      @content = JSON.parse(source, symbolize_names: true)
-
-      @content[:date] = Date.parse(@content[:date]) if @content[:date]
-
-      change_configuration if !@content[:version] || breaking_changes?(@content[:version])
-    rescue JSON::ParserError
-      @content = {}
-      change_configuration
-    end
-
-    def default_config_path
-      File.join(ENV['HOME'], '.config', 'download_tv', 'config')
-    end
-
-    ##
-    # Returns true if a major or minor update has been detected, something falsy otherwise
-    def breaking_changes?(version)
-      DownloadTV::VERSION.split('.')
-                         .zip(version.split('.'))
-                         .find_index { |x, y| y < x }
-                         &.< 2
-    end
-
-    def print_config
-      @content.each { |k, v| puts "#{k}: #{v}" }
-    end
-
-    def print_attr(arg)
-      puts @content[arg]
-    end
-
-    def clear_pending
-      @content[:pending].clear
-      serialize
-    end
-
-    def queue_pending(show)
-      @content[:pending] << show
-      serialize
+    def read_and_split_list(case_method)
+      $stdin.gets.chomp.split(',')
+            .map(&:strip)
+            .map(&case_method)
     end
   end
 end
